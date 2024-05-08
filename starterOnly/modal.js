67e1849a38bdb444cafd64c2413a259847d63cee
Nicolae ZOLTAN <nicolaezoltan.dev@gmail.com>
2024-05-08 09:18:12 +0200

    fermer la modale #1 Ajouter la fonctionnalité au bouton (x)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+// close modal 
+const close = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +21,11 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal
+function closeModal() {
+  modalbg.style.display = "none";
+}
+close.addEventListener("click", closeModal);
 
 
